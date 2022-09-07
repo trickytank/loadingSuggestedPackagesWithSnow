280b7dc68dbfa7ed6f4e48ada46312e86efb4a0c
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-07 19:40:21 +1000

    Have two functions

--- R/run_in_snow.R
+++ R/run_in_snow.R
@@ -1,14 +1,31 @@
-#' Load packages from within a snow instance test
+#' Load packages from within a snow instance test using require
 #'
 #' @examples
-#' cluster <- snow::makeCluster(1)
-#' run_in_snow(cluster)
+#' cluster_req <- snow::makeCluster(1)
+#' run_in_snow(cluster_req)
 #' @export
-run_in_snow <- function(cluster) {
+run_in_snow_require <- function(cluster) {
   if (!requireNamespace("dplyr", quietly = TRUE))
     stop("The package 'dplyr' is required to run this function.")
   snow::clusterEvalQ(cluster, {
     require("dplyr")
+  })
+  starwars_list <- list(slice_sample(starwars, n = 2), slice_sample(starwars, n = 3))
+  snow::clusterExport(cluster, c("another_function"))
+  snow::parLapply(cluster, starwars_list, simple_function)
+}
+
+#' Load packages from within a snow instance test using requireNamespace
+#'
+#' @examples
+#' cluster_rns <- snow::makeCluster(1)
+#' run_in_snow_requireNamespace(cluster_rns)
+#' @export
+run_in_snow_requireNamespace <- function(cluster) {
+  if (!requireNamespace("dplyr", quietly = TRUE))
+    stop("The package 'dplyr' is required to run this function.")
+  snow::clusterEvalQ(cluster, {
+    requireNamespace("dplyr")
   })
   starwars_list <- list(slice_sample(starwars, n = 2), slice_sample(starwars, n = 3))
   snow::clusterExport(cluster, c("another_function"))
